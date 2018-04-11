edc46baa3f627aa39821c6d7ddb407b1d9b5ffeb
yuchengc <yuchengc@umich.edu>
2018-04-10 23:19:59 -0400

    add assignment

---
+++ app.py
@@ -0,0 +1,48 @@
+# from flask import Flask, render_template
+import model
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '''
+        <img src="/static/blockM.png"/>
+        <h1>Michigan Sports Info!</h1>
+        <ul>
+            <li><a href="/bball"> Men's Basketball </a></li>
+        </ul>
+    '''
+
+@app.route('/bball')
+def bball():
+    
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_bball_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_bball_seasons()
+        
+    return render_template("seasons.html", seasons=seasons)
+
+
+
+@app.route('/hello', methods=['GET', 'POST'])
+
+def hello():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+    else:
+        firstname = ''
+        lastname = ''
+
+    return render_template("hello.html", firstname=firstname, lastname=lastname)
+
+
+if __name__ == '__main__':
+    # app.run(debug=True)
+
+	model.init_bball()
+    app.run(debug=True)
